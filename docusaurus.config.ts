b3fe16f69cedef08e6d369b715d787753bf783be
whes1015 <whes1015@gmail.com>
2024-06-14 18:37:10 +0800

    fix: url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -90,7 +90,7 @@ const config: Config = {
           items: [
             {
               label: "Swift",
-              to: "/docs/docs/swift/start/",
+              to: "./docs/swift/start/",
             },
           ],
         },
